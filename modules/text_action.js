160068666c25a326dc7c1743007e9e955c0c46ae
Avichal Rakesh <avichalrakesh@Avichals-MacBook-Air.local>
2017-02-18 17:41:52 -0600

    Cleaned Skeleton

--- modules/text_action.js
+++ modules/text_action.js
@@ -4,8 +4,6 @@ module.exports = {};
 
 module.exports.actOnText = function(assistant)  {
     console.log("Acting on text");
-    let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
-        assistant.getRawInput() + '</say-as></speak>',
-        ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    let inputPrompt = assistant.buildInputPrompt(true, assistant.getRawInput());
       assistant.ask(inputPrompt);
 }
